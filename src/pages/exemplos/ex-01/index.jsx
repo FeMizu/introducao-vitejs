31d098e2692513de2ceb3e5fe6fd7f734f16359b
FeMizu <felipe.mizuma@gmail.com>
2026-03-10 10:56:43 -0300

    aula4

---
+++ src/pages/exemplos/ex-01/index.jsx
@@ -0,0 +1,10 @@
+function Exemplo01() {
+    return (
+        <>
+            <h1>Exemplo1</h1>
+            <p>Componente básico para renderizar tela com React</p>
+        </>
+    )
+}
+
+export default Exemplo01;
